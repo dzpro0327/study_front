54bbddbc30c99b89b5601b1c27eb354b5fb46d36
이동주 <dzpro0327@gmail.com>
2021-01-14 05:24:23 +0900

    1/14

---
+++ js/custom02.js
@@ -0,0 +1,11 @@
+$('.add-btn').click(function(){
+    $('.container div').addClass('active');
+})
+
+$('.remove-btn').click(function(){
+    $('.container div').removeClass('active');
+})
+
+$('.toggle-btn').click(function(){
+    $('.container div').toggleClass('active');
+})
